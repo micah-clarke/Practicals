b8e1965dddce4e635ca2f25bf1682efc79962a11
micah-clarke <48541196+micah-clarke@users.noreply.github.com>
2019-04-11 14:16:03 +1000

    word_occurences branch

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,16 +1,16 @@
 
 text = input("Text: ")
-word_to_be_counted = {}
+word_to_frequency = {}
 
 words = text.split(' ')
 for word in words:
-    frequency = word_to_be_counted.get(word, 0)
-    word_to_be_counted[word] = frequency + 1
+    frequency = word_to_frequency.get(word, 0)
+    word_to_frequency[word] = frequency + 1
 
 
-words = list(word_to_be_counted)
+words = list(word_to_frequency)
 words.sort()
 
 max_length = max((len(word) for word in words))
 for word in words:
-    print("{:{}} : {}".format(word, max_length, word_to_be_counted[word]))
+    print("{:{}} : {}".format(word, max_length, word_to_frequency[word]))
